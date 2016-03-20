9eb3420e85811073725369f5c80eb79c523292b6
Dmitry Gorohov <Dmitgt@gmail.com>
2016-03-20 16:18:39 +0300

    Update root.py

--- root.py
+++ root.py
@@ -1,14 +1,26 @@
-getSourceFolder()
-getTargetFolder()
+getSourceFolder:
+  # Get source folder from ui or console
+  
+getTargetFolder:
+  # Get target folder from ui or console
 
-indexSourceItems()
-indexTargetItems()
+indexSourceItems:
+  # Walk through source folder and create list of contained items
 
-# item got some properties:
-#	name
-#	path
-#	filelist if its folder
-#	sync attributes
+indexTargetItems:
+  # Walk through target folder and create list of contained items  
 
-compareItems()
-copyItems()
+compareItems:
+  # Loop through items from source and target folders, and get difference
+  
+copyItems:
+  # Copy items usinig difference data 
+
+class Item:
+  # item got some properties:
+  #	name
+  #	path
+  #	filelist if its folder
+  #	sync attributes
+
+
