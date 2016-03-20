48b4f02c44b227db2b36a520751dac900a16c04c
Dmitry Gorohov <Dmitgt@gmail.com>
2016-03-20 16:08:34 +0300

    Update root.py

--- root.py
+++ root.py
@@ -1,1 +1,14 @@
+getSourceFolder()
+getTargetFolder()
 
+indexSourceItems()
+indexTargetItems()
+
+# item got some properties:
+#	name
+#	path
+#	filelist if its folder
+#	sync attributes
+
+compareItems()
+copyItems()
